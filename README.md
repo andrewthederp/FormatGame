d4c00c1c4a5ad8c5fe81533f3ff110b3e7c2e2e3
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2022-07-11 13:03:23 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+A python module that allows you to format various games
+
+---
+## Note
+Due to the amount of assets in this package, it is quite big so keep that in mind when installing
---
+++ setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+desc = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="format_game",
+    version="1.0.0",
+    description="A python module that allows you to format various games",
+    long_description=desc,
+    long_description_content_type="text/markdown",
+    author="andrew",
+    url="https://github.com/andrewthederp/FormatGame",
+    license="Apache",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    include_package_data=True,
+    package_data={
+        '': [
+            'hangman/**',
+            'fonts/**',
+            'chess/**',
+            'chess/8bit/**',
+            'chess/blue/**',
+            'chess/glass/**',
+            'chess/graffiti/**',
+            'chess/green/**',
+            'chess/light/**',
+            'chess/lolz/**',
+            'chess/metal/**',
+            'chess/neon/**',
+            'chess/newspaper/**',
+            'chess/tournament/**',
+            'chess/wood/**',
+            '2048/**'
+        ]
+    },
+    install_requires=["pillow", "colorama"],
+    python_requires=">=3.6",
+    packages=find_packages(include=["format_game", "format_game.*"]),
+)
